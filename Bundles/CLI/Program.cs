ce461ec4246f65ff3668cdf7cdd7fd08eda448cf
Norman Dankert <norman.dankert@outlook.com>
2020-09-22 13:53:35 +0200

    add initial project files for CLI and library

---
+++ Bundles/CLI/Program.cs
@@ -0,0 +1,15 @@
+﻿using PoeTools.Bundles.Lib;
+using System;
+
+namespace PoeTools.Bundles.CLI
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Hello CLI!");
+
+			Class1.Hello();
+		}
+	}
+}
