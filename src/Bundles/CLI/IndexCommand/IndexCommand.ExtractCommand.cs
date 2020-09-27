93016e13722d6745f07d9690ed7b7acb5b3fd045
Norman Dankert <norman.dankert@outlook.com>
2020-09-27 04:46:47 +0200

    add parallel processing option to extract command

--- src/Bundles/CLI/IndexCommand/IndexCommand.ExtractCommand.cs
+++ src/Bundles/CLI/IndexCommand/IndexCommand.ExtractCommand.cs
@@ -3,6 +3,7 @@ using System.CommandLine;
 using System.CommandLine.Invocation;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 using DotNet.Globbing;
 
@@ -12,13 +13,14 @@ namespace PoeTools.Bundles.CLI {
 			public ExtractCommand() : base("extract", "Extract one or multiple files from referenced bundles to the current directory") {
 				AddArgument(indexFile);
 				AddArgument(new Argument<string>("pathSpec", "Glob pattern for files to extract"));
+				AddOption(new Option<bool>(new[] { "-p", "--parallel" }, "Extract files in parallel"));
 				AddOption(new Option<bool>(new[] { "-f", "--flat" }, "Flatten the file hierarchy"));
 				AddOption(new Option<DirectoryInfo>(new[] { "-o", "--output" }, "Location to place the extracted files"));
 
-				Handler = CommandHandler.Create<FileInfo, string, bool, DirectoryInfo>(ExecuteCommand);
+				Handler = CommandHandler.Create<FileInfo, string, bool, bool, DirectoryInfo>(ExecuteCommand);
 			}
 
-			private static void ExecuteCommand(FileInfo indexFile, string pathSpec, bool flat, DirectoryInfo output) {
+			private static void ExecuteCommand(FileInfo indexFile, string pathSpec, bool parallel, bool flat, DirectoryInfo output) {
 				var globOptions = new GlobOptions();
 				globOptions.Evaluation.CaseInsensitive = true;
 				var glob = Glob.Parse(pathSpec, globOptions);
@@ -29,13 +31,22 @@ namespace PoeTools.Bundles.CLI {
 				var files = index.GetAllFilePaths().Where(path => glob.IsMatch(path));
 				Console.WriteLine($"Extracting {files.Count()} matching files");
 
-				foreach (var file in files) {
-					var content = index.GetFile(file);
-					var outputFilePath = Path.Combine(outputDirectory, flat ? Path.GetFileName(file) : file);
+				if (parallel) {
+					Parallel.ForEach(files, (file) => ExtractFile(index, file, outputDirectory, flat));
 
-					Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath));
-					File.WriteAllBytes(outputFilePath, content.ToArray());
+				} else {
+					foreach (var file in files) {
+						ExtractFile(index, file, outputDirectory, flat);
+					}
 				}
+			}
+
+			private static void ExtractFile(Lib.Index index, string file, string outputDirectory, bool flat) {
+				var content = index.GetFile(file);
+				var outputFilePath = Path.Combine(outputDirectory, flat ? Path.GetFileName(file) : file);
+
+				Directory.CreateDirectory(Path.GetDirectoryName(outputFilePath));
+				File.WriteAllBytes(outputFilePath, content.ToArray());
 			}
 		}
 	}
